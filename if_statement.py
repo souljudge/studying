3958c8706ae7a380ae6b02f25947b8a425452c12
souljudge <61267824+souljudge@users.noreply.github.com>
2020-02-20 16:31:22 +0800

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,7 @@
+age=input("please input your age:")
+print(type(age))
+#notice:after you input a number,the type of 'age' will be string rather than integer you expected
+#注意：当你输入一个数字后，age的类型是字符串而不是整数
+#so if you want to make the type integer,follow the operations below
+age=int(input("please input your age:"))
+print(type(age))
